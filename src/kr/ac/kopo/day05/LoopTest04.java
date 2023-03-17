3e7d8b37abcfeb164f1fe2bb2f4305986214a039
Kim BoHyeong <kerong12@naver.com>
2023-03-17 13:16:55 +0900

    for 문 연습

--- src/kr/ac/kopo/day05/LoopTest04.java
+++ src/kr/ac/kopo/day05/LoopTest04.java
@@ -4,65 +4,115 @@ public class LoopTest04 {
 
 	public static void main(String[] args) {
 		
-/*		int i,j;    1~9번문제
+/*		int i,j;   
 		for(i = 1; i <= 5; i++ ) {
 			for(j = 1; j <=5  ; j++) {
 			System.out.print("*");
-			System.out.println();		
-		}
-*/
+			} System.out.println();
+*/  					
+/*
+*****
+*****
+*****
+*****
+*****			
+ */
+
 /*		int i,j; 
 		for(i = 1; i <=5; i++) {
 			for(j =1; j <=5; j++) {
 				System.out.print(i);
 			} System.out.println();			
 		}
-*/	
+*/
+/*
+11111
+22222
+33333
+44444
+55555
+*/
 /*		int i,j;
 		for(i=1; i <=5; i++) {
 			for(j=1; j<=5; j++) {
 				System.out.print(j);
 			} System.out.println();
 		}
-*/		
+*/
+/*
+12345
+12345
+12345
+12345
+12345 		
+ */
 /*		int i,j;
 		for(i=0; i<=4; i++) {
 			for(j=1; j<=5; j++) {
 				System.out.print(j+i);
 			} System.out.println();
-*/			
-/* 		int i,j;
+		}
+*/	
+/*
+12345
+23456
+34567
+45678
+56789		
+ */		
+ /*		int i,j;
 		for(i=1; i<=5; i++) {
 			for(j=i; j<=i+4; j++) {
 				System.out.print(j);
 			} System.out.println();
-*/
+		}
+*/		
 /*		int i,j;
 		for(i=0; i<=4; i++) {
 			for(j=5; j<=9; j++) {
 				System.out.print(j-i);
 			} System.out.println();
-*/		
-		int i,j;
+		}
+*/	
+/*
+56789
+45678
+34567
+23456
+12345
+ */
+/*	int i,j;
 		for(i=1; i<=5; i++) {
 			for(j=i; j<=i+4; j++) {
 				System.out.print(j);
 			} System.out.println();
-		
+*/		
 /*		int i,j;
 		for(i=1; i<=5; i++) {
 			for(j=1; j<=i; j++) {
 				System.out.print("*");
-			} System.out.println();
-				
+			} System.out.println();	
 			}
 */
-/*		int i,j;
+/*
+*
+**
+***
+****
+*****
+ */
+		int i,j;
 		for(i=1; i<=5; i++) {
 			for(j=1; j<=6-i; j++) {
 				System.out.print("*");
 			} System.out.println();
-*/			
-		}
+		}			
+/*
+*****
+****
+***
+**
+*		
+ */
 	}
 }
