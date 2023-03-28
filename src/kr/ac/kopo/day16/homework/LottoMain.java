2e5e5bc428c2662aac3276cbe65339307638606c
Kim BoHyeong <kerong12@naver.com>
2023-03-28 17:27:51 +0900

    로또프로그램 만들기

---
+++ src/kr/ac/kopo/day16/homework/LottoMain.java
@@ -0,0 +1,23 @@
+package kr.ac.kopo.day16.homework;
+
+/*
+	게임수를 입력 : 10
+	게임1 : 
+	게임2 :
+	게임3 :
+	...
+	게임10 :
+ */
+
+
+
+public class LottoMain {
+
+	public static void main(String[] args) {
+		
+		LottoView lotto = new LottoView();
+		lotto.start();
+		
+	}
+
+}
---
+++ src/kr/ac/kopo/day16/homework/LottoView.java
@@ -0,0 +1,26 @@
+package kr.ac.kopo.day16.homework;
+
+
+public class LottoView {
+	
+	public void start() {
+		
+//		System.out.println("게임수를 입력 : );
+		int gameCnt = 10;
+		for(int i = 1; i <= gameCnt; i++) {
+			System.out.println("게임  " + i + " : " + Arrays.toString(getLotto());
+		}
+		
+		private int[] getLotto() {
+			int[] lottoNums = new int[6];
+			
+			return lottoNums;
+			
+		}
+		
+		
+		
+		
+	}
+
+}
